c3a6de4ed9888af54087e2198f212b07af8804ac
chintan-panchal <chintan.panchal@jiniguru.net>
2020-05-28 11:15:42 +0530

    refactoring

--- src/com/excel/LeapYear.java
+++ src/com/excel/LeapYear.java
@@ -9,15 +9,14 @@ package com.excel;
 public class LeapYear {
 
     public boolean isLeapYear(int year) {
+        boolean isLeapYear = false;
         if (year % 100 == 0) {
             if (year % 400 == 0) {
-                return true;
+                isLeapYear = true;
             }
-            return false;
-        } else if (year % 4 == 0 && year % 100 != 0) {
-            return true;
+        } else if (year % 4 == 0) {
+            isLeapYear = true;
         }
-        return false;
+        return isLeapYear;
     }
-
 }
